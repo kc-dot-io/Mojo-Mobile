ca7f0b5c6892062e19875199da67319f2f184cb7
Kyle Campbell <mail@slajax.com>
2011-07-20 23:00:28 -0700

    Built out a proof of concept work flow for model, controller, view

---
+++ Resources/mojo/models/facebook.js
@@ -0,0 +1,19 @@
+
+Mojo.Ready('facebook','model',function(options){
+  
+  Ti.API.info('Model got these options: ');
+  Ti.API.info(options);
+  
+});
+
+var exports = { 
+  data:[
+    {
+     name: 'test' 
+    },
+    {
+     name: 'test2'
+    }
+  ]
+}; 
+
---
+++ Resources/mojo/views/facebook.js
@@ -0,0 +1,12 @@
+
+Mojo.Ready('facebook','view',function(options){
+  
+  Ti.API.info('View got these options: ');
+  Ti.API.info(options);
+  
+});
+
+var exports = { view_export: '1' };
+
+
+
